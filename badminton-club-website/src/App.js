39c86764561047f0ca729ab372d0d46cac3bd1ef
cqtqlyst <aayush.gaywala@gmail.com>
2023-06-21 16:06:26 -0700

    working on nav bar

--- badminton-club-website/src/App.js
+++ badminton-club-website/src/App.js
@@ -1,8 +1,15 @@
+import "./App.css";
+import NavBar from "./Navbar";
 
 function App() {
-
-  return <div> Hello </div>;
-
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+      </header>
+      <body className="App-body"></body>
+    </div>
+  );
 }
 
 export default App;
