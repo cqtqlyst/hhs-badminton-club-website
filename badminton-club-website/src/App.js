4dbd36c7260c090de6d7e1ec57066d913b39c7a2
cqtqlyst <aayush.gaywala@gmail.com>
2023-06-22 15:37:16 -0700

    routing with navbar done

--- badminton-club-website/src/App.js
+++ badminton-club-website/src/App.js
@@ -1,13 +1,31 @@
 import "./App.css";
 import NavBar from "./components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import ForMembers from "./pages/ForMembers";
 
 function App() {
+  let component;
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home />;
+      break;
+    case "/AboutUs":
+      component = <About />;
+      break;
+    case "/ForMembers":
+      component = <ForMembers />;
+      break;
+    default:
+      component = <Home />;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
       </header>
-      <body className="App-body"></body>
+      <body className="App-body"> {component}</body>
     </div>
   );
 }
