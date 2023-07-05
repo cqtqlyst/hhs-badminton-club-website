a61d623bae54a17392eb954bcc5f66543cb4daa7
sbalaji524 <90276144+sbalaji524@users.noreply.github.com>
2023-07-05 15:03:55 -0700

    added images

--- badminton-club-website/src/pages/Home.js
+++ badminton-club-website/src/pages/Home.js
@@ -1,5 +1,48 @@
-function Home() {
-  return <p>Home</p>;
+import InstLogo from "../images/instlogo.png";
+import Logo1 from "../images/Logo1.png";
+import React, { useState, useEffect } from 'react';
+
+function Home() { 
+  const images = [
+    InstLogo, Logo1
+    // Add more image URLs here
+  ];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    // This function will be called every 5 seconds (5000 milliseconds)
+    const interval = setInterval(() => {
+      // Calculate the next image index by incrementing currentImageIndex
+      const nextImageIndex = (currentImageIndex + 1) % images.length;
+      setCurrentImageIndex(nextImageIndex);
+    }, 3000); // 5000 milliseconds = 5 seconds
+
+    // Clean up the interval to avoid memory leaks
+    return () => clearInterval(interval);
+  }, [currentImageIndex, images.length]);
+
+  
+  return (
+    <div class = "info">
+          <div class = "text">
+            <p>What is our Badminton Club?</p>
+            <p>Our Badminton Club is a place where people who enjoy the sport of badminton can come to meet and talk about different things. 
+            We host meetings triweekly where we discuss different things such as where to train, details of the upcoming season, and more.
+            Additionally, every month we plan to host an open gym where anyone can come in and play. We limit the amount of people for each open gym to 25 people so everyone can play.
+            You can sign up for these open gyms on the events page of this website. Towards the end of both semesters, we will also hold a torunament with rewards.</p>
+          </div>
+
+          <div class = "content">
+            <img src={images[currentImageIndex]} alt="Badminton Club" style={{ width: '400px', height: '300px'}} />
+          </div>
+    </div>
+    
+  );
+}
+
+function temp() {
+  console.log("hello")
 }
 
 export default Home;
